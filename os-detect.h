0d954e8ee1ec31d4fb3ecc1cbeeea3313289b36c
Amirali Dashti <devoft1@gmail.com>
2025-05-19 02:09:45 +0330

    Delete os-detect.h

--- os-detect.h
+++
@@ -1,9 +0,0 @@
-#ifndef OS_DETECT_H
-#define OS_DETECT_H
-
-// Function to determine the base operating system
-// Returns a string literal (e.g., "debian", "arch", "fedora", "suse", "rhel", "unknown")
-// The caller should not free the returned string.
-const char* get_os_base_name();
-
-#endif // OS_DETECT_H
